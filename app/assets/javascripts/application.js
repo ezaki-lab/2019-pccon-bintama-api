584a53193169ebf47f21177f4a6cc02eeafd9eed
弓削隼大 <18440@toba.kosen-ac.jp>
2019-08-13 04:18:28 +0900

    メインページを書いた

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,11 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+(function() {
+  var burger = document.querySelector('.burger');
+  var menu = document.querySelector('#'+burger.dataset.target);
+  burger.addEventListener('click', function() {
+      burger.classList.toggle('is-active');
+      menu.classList.toggle('is-active');
+	});
+})();
