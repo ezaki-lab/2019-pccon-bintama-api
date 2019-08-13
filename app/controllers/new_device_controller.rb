a4c49816ce671077aa95f52f984d3a963bb3dd6d
弓削隼大 <18440@toba.kosen-ac.jp>
2019-08-14 01:46:20 +0900

    デバイス登録ページを実装

--- app/controllers/new_device_controller.rb
+++ app/controllers/new_device_controller.rb
@@ -1,2 +1,23 @@
 class NewDeviceController < ApplicationController
+	def index
+	end
+
+	def new
+		@device = Device.new
+	end
+					
+	def create
+		@device = Device.find_by(token: params[:device][:token])
+		if @device == nil
+			flash[:notice] = "そのトークンは存在しません"
+			redirect_to "/new_device/new"
+		else
+			@new_device = DeviceUser.new
+			@new_device.user_id = session[:user_id]
+			@new_device.device_id = @device.id
+			@new_device.save
+			flash[:notice] = "デバイス登録完了"
+			redirect_to "/new_device"			
+		end		
+	end
 end
