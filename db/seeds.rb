ba8150916bf166ca7a2cab74e17ef6f1d81a1509
弓削隼大 <18440@toba.kosen-ac.jp>
2019-08-14 01:37:04 +0900

    初期データの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Device.create(
+	[
+		{
+			token: 'test1',
+			url: 'http://test1'
+		},
+		{
+			token: 'test2',
+			url: 'http://test2'
+		},
+		{
+			token: 'test3',
+			url: 'http://test3'
+		}
+	]
+)
+
+DevicePlace.create(
+	[
+		{
+			device_id: 1,
+			latitude: '123',
+			longitude: '25'
+		},
+		{
+			device_id: 2,
+			latitude: '32',
+			longitude: '192'
+		},
+		{
+			device_id: 3,
+			latitude: '143',
+			longitude: '125'
+		}
+	]
+)
