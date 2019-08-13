b61ad4c66006aa175687bd48c7cc51432cc06f6d
弓削隼大 <18440@toba.kosen-ac.jp>
2019-08-13 16:58:51 +0900

    ユーザー登録のバリデーションを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :password, confirmation: true
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
 end
