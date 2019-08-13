11b1bb0294aae3fed63fc5448661199ea300d44f
弓削隼大 <18440@toba.kosen-ac.jp>
2019-08-13 16:58:39 +0900

    ユーザー登録画面を実装

--- app/controllers/new_user_controller.rb
+++ app/controllers/new_user_controller.rb
@@ -1,2 +1,23 @@
 class NewUserController < ApplicationController
+def index
+  @new_user = User.new
+end
+        
+def create
+  @user = User.new(users_params)
+
+  if @user.save
+    redirect_to "/user_login"
+  else
+    flash[:notice] = "入力された値に間違いがあります。"
+    redirect_to "/new_user/"
+  end
+        
+end
+        
+private
+def users_params
+  params.require(:user).permit(:user_id, :email, :password, :password_confirmation)
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :main, only: [:index]
   resources :user_login, only: [:index]
+  resources :new_user, only: [:index, :create]
 end
