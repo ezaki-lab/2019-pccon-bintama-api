002468412f157a378fca3c710e97a76831549796
弓削隼大 <18440@toba.kosen-ac.jp>
2019-09-03 19:51:54 +0900

    ユーザー管理画面を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+	before_action :authenticate_user, only: [:index]
+	def index
+		@user = User.find_by(user_id: session[:user_id])
+		devices = DeviceUser.where(user_id: session[:user_id]).pluck(:device_id)
+		@device = Device.where(id: devices).pluck(:id, :token, :url, :updated_at)
+	end
 end
